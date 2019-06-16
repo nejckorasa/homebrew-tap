a823e1580ef17faae4b035e2a72f3ab1211f079e
Nejc Korasa <nejc.korasa@gmail.com>
2019-06-16 21:10:36 +0100

    Upgrade dockergo

--- Formula/dockergo.rb
+++ Formula/dockergo.rb
@@ -1,9 +1,9 @@
 class Dockergo < Formula
   desc "Script to make your life easier starting/restarting/killing Docker daemon on MacOS"
   homepage "https://github.com/nejckorasa/mac-docker-go"
-  url "https://github.com/nejckorasa/mac-docker-go/archive/v1.0.tar.gz"
-  version "1.0"
-  sha256 "12b66bfb6dd71c355b26d72e966db3245b1396fb138fa570bceb077bced81def"
+  url "https://github.com/nejckorasa/mac-docker-go/archive/v1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "0c88fbf45faa29ea99e95908177619428b9de80704c4544321535656d16a3f4d"
 
   def install
     bin.install "dockergo"
