2079e2206c8c38be17c97369c32588b1b2b23455
nejckorasa <nejc.korasa@gmail.com>
2020-06-13 12:17:08 +0100

    Upgrade dckr to v1.1.2

--- Formula/dckr.rb
+++ Formula/dckr.rb
@@ -1,9 +1,9 @@
 class Dckr < Formula
   desc "Script to make your life easier with Docker daemon on MacOS - start/stop/remove/kill/prune daemon, containers, images, volumes..."
   homepage "https://github.com/nejckorasa/mac-docker-go"
-  url "https://github.com/nejckorasa/mac-docker-go/archive/v1.1.1.tar.gz"
-  version "1.1.1"
-  sha256 "781fc83336728429faf1f5ac8cb68d0cba85468cc67c7c50addeed9509dea382"
+  url "https://github.com/nejckorasa/mac-docker-go/archive/v1.1.2.tar.gz"
+  version "1.1.2"
+  sha256 "51cf95ceb84bd6612c93125b9a4fef75a953303a2122e26acf5d4cdb3754c41a"
 
   def install
     bin.install "dckr"
